f6e117b9415202acbebb8c415c9271d2b599b656
coolepicusername <102439561+coolepicusername@users.noreply.github.com>
2022-12-04 18:49:43 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -37,6 +37,9 @@ let john = sprites.create(img`
 
 let gravity = 5
 let direction = [1,1]
+let speed = [20,20]
+let stun = [0,0]
+let primCoolDown = [0,0]
 
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(joe)
@@ -156,40 +159,43 @@ game.onUpdate(function() {
     joe.vy += gravity
     john.vy += gravity
 
-    if ((controller.player1.isPressed(ControllerButton.Left)) && (joe.vx > -100)) {
-        joe.vx -= 20
-    } else if ((controller.player1.isPressed(ControllerButton.Right)) && (joe.vx < 100)) {
-        joe.vx += 20
+    if ((controller.player1.isPressed(ControllerButton.Left)) && (joe.vx > -speed[0] * 5)) {
+        joe.vx -= speed[0]
+    } else if ((controller.player1.isPressed(ControllerButton.Right)) && (joe.vx < speed[0] * 5)) {
+        joe.vx += speed[0]
     } else {
         if (joe.vx > 0) {
-            joe.vx -= 20
+            joe.vx -= speed[0]
         }
         if (joe.vx < 0) {
-            joe.vx += 20
+            joe.vx += speed[0]
         }
     }
     
-    if ((controller.player2.isPressed(ControllerButton.Left)) && (john.vx > -100)) {
-        john.vx -= 20
-    } else if ((controller.player2.isPressed(ControllerButton.Right)) && (john.vx < 100)) {
-        john.vx += 20
+    if ((controller.player2.isPressed(ControllerButton.Left)) && (john.vx > -speed[1] * 5)) {
+        john.vx -= speed[1]
+    } else if ((controller.player2.isPressed(ControllerButton.Right)) && (john.vx < speed[1] * 5)) {
+        john.vx += speed[1]
     } else {
         if (john.vx > 0) {
-            john.vx -= 20
+            john.vx -= speed[1]
         }
         if (john.vx < 0) {
-            john.vx += 20
+            john.vx += speed[1]
         }
     }
 })
 
 controller.player1.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function() {
 if (joe.isHittingTile(CollisionDirection.Bottom)){
-    joe.vy -= 100  
+    joe.vy -= speed[0] * 5
 }
 })
 controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
     if (john.isHittingTile(CollisionDirection.Bottom)) {
-        john.vy -= 100
+        john.vy -= speed[1] * 5
     }
+})
+controller.player1.A.onEvent(ControllerButtonEvent.Pressed, function() {
+    let 
 })
